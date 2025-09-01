88b3389b2f2a7fec8161b1e110941095248b723b
Vaishnavi Walke <walkevaishu37@gmail.com>
2025-09-01 12:18:58 +0530

    Department Entity

---
+++ MyWebApi/Core/entity/Departments.cs
@@ -0,0 +1,24 @@
+﻿using MyWebApi.Core.entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace MyWebApi.Entity
+{
+    [Table("Departments")]
+    public class Departments
+    {
+        [Key]
+        public int DepartmentID { get; set; }
+
+        [Required]
+        public string? DepartmentName { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public int CreatedById { get; set; }
+
+        [JsonIgnore]
+        public Users? user { get; set; }
+    }
+}
