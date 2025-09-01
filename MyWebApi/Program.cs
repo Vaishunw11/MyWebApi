6be8e972c6ada683aa64b9580efdaa7b3b2e1621
Vaishnavi Walke <walkevaishu37@gmail.com>
2025-09-01 14:18:25 +0530

    Update All

--- MyWebApi/Program.cs
+++ MyWebApi/Program.cs
@@ -1,25 +1,43 @@
+using Authorization.Service;
+using Microsoft.EntityFrameworkCore;
+using MyWebApi.BI.Mapping;
+using MyWebApi.Core.Contract;
+using MyWebApi.Data.DAL.AppDB;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<UserMapping>();
+builder.Services.AddScoped<IDeptRepo, DeptRepository>();
+builder.Services.AddScoped<IDeptService, DeptService>();
+builder.Services.AddScoped<Deptmapping>();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
+
